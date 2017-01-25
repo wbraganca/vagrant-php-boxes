837fb54d473d7988348a88092ae3456dfb3ebdb3
Wanderson Bragança <wanderson.wbc@gmail.com>
2017-01-25 16:47:17 -0200

    Fix installation of MySQL 5.7

--- scripts/install-mysql.sh
+++ scripts/install-mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 install_mysql_57() {
-  wget --quiet -P /tmp http://dev.mysql.com/get/mysql-apt-config_0.5.3-1_all.deb
+  wget --quiet -P /tmp http://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb
 
   debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-router select none'
   debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-connector-python select none'
@@ -9,7 +9,7 @@ install_mysql_57() {
   debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
   debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-mysql-utilities select mysql-utilities-1.5'
 
-  dpkg -i /tmp/mysql-apt-config_0.5.3-1_all.deb > /dev/null 2>&1
+  dpkg -i /tmp/mysql-apt-config_0.8.1-1_all.deb > /dev/null 2>&1
   apt-get update > /dev/null 2>&1
 
   debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password 123456"
@@ -25,9 +25,12 @@ install_mysql_57() {
   mysql -e "UPDATE mysql.user SET Host='%', plugin='mysql_native_password', authentication_string=password('123456') WHERE user='root';"
   mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=123456 --force mysql
 
-  rm -rf /tmp/mysql-apt-config_0.5.3-1_all.deb
-}
+  rm -rf /tmp/mysql-apt-config_0.8.1-1_all.deb
 
+  service mysql restart &> /dev/null
+  mysql -uroot -p123456 -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED By '123456';" &> /dev/null
+  mysql -uroot -p123456 -e "FLUSH PRIVILEGES;" &> /dev/null
+}
 
 install_mysql_55() {
   debconf-set-selections <<< "mysql-server mysql-server/root_password password 123456"
